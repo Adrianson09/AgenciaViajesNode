779ce02cfffa2f3a72b5512a167b5b8ded4ce805
Adrian Guerrero <adrianson@outlook.com>
2023-07-27 14:28:56 -0600

    Install PUG and CSS/IMGs

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 import express from 'express';
+import router from './routes/index.js'
 
 const app = express();
 
 // Definir Puerto
 const port = process.env.PORT || 4000
 
-app.get('/',(req, res) => { //--------------> req - lo que enviamos : res - lo que express nos responde
-res.send('Inicio');
-})
-app.get('/nosotros',(req, res) => { //--------------> req - lo que enviamos : res - lo que express nos responde
-res.send('Nosotros');
-})
-app.get('/contacto',(req, res) => { //--------------> req - lo que enviamos : res - lo que express nos responde
-res.send('Contacto');
-})
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Agrega Router
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`El servidor está funcionando en el puerto ${port}`);
