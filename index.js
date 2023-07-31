dceae4fcd4f5f20940116149d9711a090eea11b5
Adrian Guerrero <adrianson@outlook.com>
2023-07-31 13:17:13 -0600

    Install PUG and CSS/IMGs

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 4000
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+// Definir la carpeta Publica
+app.use(express.static('public'));
+
 // Agrega Router
 app.use('/', router)
 
