c88ea7180604a462ddd4af7b5ac21c2b7d60c560
Adrian Guerrero <adrianson@outlook.com>
2023-08-02 13:25:58 -0600

    add environment variables

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv'
 
-const db = new Sequelize('agencia', 'root', '', {
-    host: '127.0.0.1',
-    port: '3306',
+dotenv.config()
+console.log(process.env.DB_HOST)
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
